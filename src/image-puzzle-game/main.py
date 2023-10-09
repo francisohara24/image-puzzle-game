60f0f39dc97d1131acc283e9ea80c79f798e3cb7
francisohara24 <franciskohara@gmail.com>
2023-10-08 23:59:09 -0400

    Chore: update project structure

---
+++ src/image-puzzle-game/main.py
@@ -0,0 +1,31 @@
+from puzzle import Puzzle
+
+
+def play():
+    """Start the main game sequence"""
+
+    # Welcome the user to the game
+    # create animated text effect using time.sleep() define as helper function
+    print("Welcome to the puzzle Game!")
+
+    # Instantiate puzzle and display to user
+    game = Puzzle("../../data/puzzle_img_1.jpg")
+    game.render_puzzle()
+    player_won = False
+
+    while not player_won:
+        # Ask user which tile they would like to move and to what destination
+        user_tile = input("What tile would you like to move?\t")
+        user_destination = input("Where would you like to move it to")
+
+        # move tile and update empty slot and movable tiles
+        moved = game.move(int(user_tile), int(user_destination))
+        if not moved:
+            print("Choose a valid tile\n")
+        else:
+            game.render_puzzle()
+
+        # check if player won
+        player_won = game.check_if_player_won()
+
+    game.start_win_sequence()
